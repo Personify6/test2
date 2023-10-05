a591c7de7c54663b66795d24de9a28269313cb49
David373289 <brightlylearning@gmail.com>
2023-10-04 21:47:10 -0600

    Read changelog in releases section

--- scripts/settings.js
+++ scripts/settings.js
@@ -3,7 +3,7 @@
 */
 
 
-var version = "1.6";
+var version = "1.6.1";
 // This is executed before the DOM is ready
 var darkModeToggle = localStorage.getItem("dark-mode");
 var turnOn = document.getElementById("dark-yes");
