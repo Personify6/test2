383d665117a227290add340c3658a9546a3bb776
Personify6 <56610925+Personify6@users.noreply.github.com>
2024-10-07 07:15:26 -0600

    Update version

--- scripts/settings.js
+++ scripts/settings.js
@@ -3,7 +3,7 @@
 */
 
 
-var version = "1.6.2";
+var version = "1.6.3";
 // This is executed before the DOM is ready
 var darkModeToggle = localStorage.getItem("dark-mode");
 var turnOn = document.getElementById("dark-yes");
